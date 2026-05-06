52fee4d9ec4fb0013c80f25ceecbb13a2e3163e3
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2026-05-06 13:30:41 -0600

    Refactor SQL script: enhance comments, remove redundant selects, and add view for movie rentals

--- lab-01/query.sql
+++ lab-01/query.sql
@@ -3,8 +3,7 @@
 select *
   from mm_member;
 
--- Add new member
--- begin
+-- Add a new member, update the credit card number for that member, and then delete that member.
 insert into mm_member (
    member_id,
    last,
@@ -13,24 +12,76 @@ insert into mm_member (
            'Smith',
            'John' );
 
-select *
-  from mm_member;
-
 update mm_member
    set
    credit_card = '441120453336'
  where member_id = 15;
 
-select *
-  from mm_member;
--- Remove membership
-
 delete from mm_member
  where member_id = 15;
 
-select *
-  from mm_member;
+-- Display the title of each movie, the rental ID, and the last names of all members who have rented those movies.
 
--- Commit changes
+select m.movie_title,
+       r.rental_id,
+       mm.last
+  from mm_member mm
+  join mm_rental r
+on mm.member_id = r.member_id
+  join mm_movie m
+on r.movie_id = m.movie_id
+ order by r.rental_id;
 
-commit;
+select m.movie_title,
+       r.rental_id,
+       mm.last
+  from mm_member mm,
+       mm_rental r,
+       mm_movie m
+ where mm.member_id = r.member_id
+   and r.movie_id = m.movie_id;
+
+-- Custom table and sequence
+
+create table my_table (
+   my_number number,
+   my_date   date,
+   my_string varchar2(5)
+);
+
+create sequence my_seq start with 20 increment by 2 nomaxvalue nocycle;
+
+select last_number,
+       increment_by
+  from user_sequences us
+ where sequence_name = 'MY_SEQ';
+
+select my_seq.nextval
+  from dual;
+
+alter sequence my_seq increment by 5;
+
+select my_seq.nextval
+  from dual;
+
+insert into mm_movie (
+   movie_id,
+   movie_title,
+   mm_movie.movie_cat_id
+) values ( my_seq.nextval,
+           'The Matrix',
+           1 );
+
+
+create view vw_movie_rental as
+   select m.movie_title,
+          r.rental_id,
+          mm.last
+     from mm_member mm
+     join mm_rental r
+   on mm.member_id = r.member_id
+     join mm_movie m
+   on r.movie_id = m.movie_id
+    order by r.rental_id;
+
+select * from vw_movie_rental;
